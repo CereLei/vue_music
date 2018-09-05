ab87956492240097877e10c3092533a7267b1fe9
zhaolei <983361306@qq.com>
2018-09-05 09:48:09 +0800

    导航

--- music/src/App.vue
+++ music/src/App.vue
@@ -1,15 +1,19 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tab></tab>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
 import mHeader from 'components/m-header/m-header'
+import Tab from 'components/tab/tab'
 export default {
   name: 'App',
   components:{
-    mHeader
+    mHeader,
+    Tab
   }
 }
 </script>
