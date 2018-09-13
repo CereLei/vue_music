ac2b0021379446db12d8e3cb0ce79c157af63878
zhaolei <983361306@qq.com>
2018-09-13 09:01:56 +0800

    快速字母查询

--- music/src/base/scroll/scroll.vue
+++ music/src/base/scroll/scroll.vue
@@ -16,6 +16,10 @@ export default {
             type:Boolean,
             default:true
         },
+        listenScroll: {
+        type: Boolean,
+        default: false
+      },
         data:{
             type:Array,
             default:null
@@ -36,6 +40,15 @@ export default {
                 probeType:this.probeType,
                 click:this.click
             })
+            console.log(this.listenScroll)
+            if(this.listenScroll){
+                let me =this;
+                
+                this.scroll.on('scroll',(pos)=>{
+                    
+                    me.$emit('scroll',pos)
+                })
+            }
         },
         enable(){
             this.scroll && this.scroll.enable()
@@ -45,6 +58,12 @@ export default {
         },
         refresh(){
             this.scroll && this.scroll.refresh()
+        },
+        scrollTo(){
+            this.scroll &&this.scroll.scrollTo.apply(this.scroll,arguments)
+        },
+        scrollToElement(){
+            this.scroll && this.scroll.scrollToElement.apply(this.scroll,arguments)
         }
     },
     watch:{
